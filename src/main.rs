190e7ae88fd8bf0004944ff6034c04fd6249c16f
Luna <luna.wilkes@gmail.com>
2024-04-01 16:21:14 +1300

    Add error handling to Transaction struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::fmt::Error;
+use std::{error::Error, fmt};
+use std::fmt::Formatter;
 use chrono::{DateTime, Local};
 
-fn main() {
-    println!("Hello, world!");
-}
+fn main() {}
 
 
 struct Transaction {
@@ -13,11 +12,35 @@ struct Transaction {
 }
 
 impl Transaction {
-    pub fn new(label: &str, amount: i32) -> Result<Transaction, Error> {
+    pub fn new(label: &str, amount: i32) -> Result<Transaction, TransactionCreationError> {
+        if label.is_empty() {
+            return Err(TransactionCreationError::new("No name provided for transaction"));
+        }
         Ok(Transaction {
             label: String::from(label),
             amount,
             time: Local::now(),
         })
+    }
+}
+
+#[derive(Debug)]
+struct TransactionCreationError {
+    reason: String,
+}
+
+impl TransactionCreationError {
+    pub fn new(reason: &str) -> TransactionCreationError{
+        TransactionCreationError {
+            reason: String::from(reason),
+        }
+    }
+}
+
+impl Error for TransactionCreationError {}
+
+impl fmt::Display for TransactionCreationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.reason)
     }
 }
