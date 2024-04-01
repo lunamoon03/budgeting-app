ab37b0df469189a9f54c8d365e47803198811f66
Luna <luna.wilkes@gmail.com>
2024-04-01 16:05:15 +1300

    Basic Transaction struct added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fmt::Error;
+use chrono::{DateTime, Local};
+
 fn main() {
     println!("Hello, world!");
+}
+
+
+struct Transaction {
+    label: String,
+    amount: i32,
+    time: DateTime<Local>,
+}
+
+impl Transaction {
+    pub fn new(label: &str, amount: i32) -> Result<Transaction, Error> {
+        Ok(Transaction {
+            label: String::from(label),
+            amount,
+            time: Local::now(),
+        })
+    }
 }
