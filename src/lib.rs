85d3681f95f0d4176c7c081bce26a2f5caf580f1
Luna <luna.wilkes@gmail.com>
2024-06-10 18:35:43 +1200

    Extracted processing of accounts from main file processing function

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ mod account;
 pub fn run(file_path: &str) -> Result<(), Box<dyn Error>>{
     let contents = get_file_contents(file_path)?;
 
-    let _accounts = read_file(contents);
+    let _accounts = process_file(contents);
 
     Ok(())
 }
@@ -16,12 +16,12 @@ pub fn run(file_path: &str) -> Result<(), Box<dyn Error>>{
 pub fn parse_args(args: &[String]) -> Result<&str, Box<dyn Error>> {
     let file_type = ".csv";
     if args.len()!=2 {
-        return Err(Box::from("Enter only one argument: [filename.csv]"));
+        return Err(Box::from(format!("Enter only one argument: [filename{}]",file_type)));
     };
 
     let file_path = match args.get(1) {
         Some(file_path) if file_path.ends_with(file_type) => file_path,
-        _ => return Err(Box::from("Enter a valid filename: [filename.csv]")),
+        _ => return Err(Box::from(format!("Enter a valid file name: [filename{}]",file_type))),
     };
 
     Ok(file_path)
@@ -38,7 +38,7 @@ fn get_file_contents(file_path: &str) -> Result<String, Box<dyn Error>> {
     Ok(contents)
 }
 
-fn read_file(contents: String) -> Result<Vec<Account>, Box<dyn Error>> {
+fn process_file(contents: String) -> Result<Vec<Account>, Box<dyn Error>> {
     if contents.is_empty() {
         return Ok(Vec::new());
     }
@@ -50,49 +50,52 @@ fn read_file(contents: String) -> Result<Vec<Account>, Box<dyn Error>> {
     }
 
     let mut accounts: Vec<Account> = Vec::new();
+    let mut account_holder: Account;
     let mut iter = 0;
     while iter < split.len()  {
+        (account_holder,iter) = process_account(&split, iter)?;
 
-        // unwrap safe as we know that iter is at most at very last spot at start of loop
-        let account_name = split.get(iter).unwrap().trim_end_matches("{").trim();
-
-        let mut account = Account::new(account_name);
-
-        iter += 1; // transaction amount OR "}"
-
-        while iter < split.len()-2 && !&split[iter].eq("}") {
-
-            let transaction_amount: f32 = match split.get(iter) {
-                Some(slice) => match slice.parse() {
-                    Ok(val) => val,
-                    _ => return Err(Box::from(format!("Value {} not valid", slice)))
-                },
-                _ => break
-            };
-
-            iter += 1; // transaction name
-
-            let transaction_name = match split.get(iter){
-                Some(slice) => slice,
-                _ => break
-            };
-
-            iter += 1; // next token (should be "}")
-
-            if let Err(e) = account.add_transaction(transaction_name, transaction_amount) {
-                return Err(e.into())
-            }
-        }
-        if !&split[iter].eq("}") {
-            return Err(Box::from("Malformed file"))
-        }
-
-        iter +=1;
-
-        accounts.push(account);
+        accounts.push(account_holder);
     }
 
     Ok(accounts)
+}
+
+fn process_account(split: &Vec<&str>, mut iter: usize) -> Result<(Account,usize), Box<dyn Error>> {
+// unwrap safe as we know that iter is at most at very last spot at start of loop
+
+    let mut account = Account::new(split.get(iter).unwrap().trim_end_matches('{').trim());
+
+    iter += 1; // transaction amount OR "}"
+
+    while iter < split.len() - 2 && !&split[iter].eq("}") {
+        let transaction_amount: f32 = match split.get(iter) {
+            Some(slice) => match slice.parse() {
+                Ok(val) => val,
+                _ => return Err(Box::from(format!("Value {} not valid", slice)))
+            },
+            _ => break
+        };
+
+        iter += 1; // transaction name
+
+        let transaction_name = match split.get(iter) {
+            Some(slice) => slice,
+            _ => break
+        };
+
+        iter += 1; // next token (should be "}")
+
+        if let Err(e) = account.add_transaction(transaction_name, transaction_amount) {
+            return Err(Box::from(e))
+        }
+    }
+    if !&split[iter].eq("}") {
+        return Err(Box::from("Malformed file"))
+    }
+
+    iter += 1;
+    Ok((account, iter))
 }
 
 #[cfg(test)]
@@ -103,31 +106,31 @@ mod tests {
     #[test]
     fn empty_file() {
         let empty = get_file_contents("src/test-files/empty.csv").unwrap();
-        assert!(read_file(empty).unwrap().is_empty());
+        assert!(process_file(empty).unwrap().is_empty());
     }
 
     #[test]
     #[should_panic]
     fn bad_start() {
-        let _ = read_file(get_file_contents("src/test-files/bad-start.csv").unwrap()).unwrap();
+        let _ = process_file(get_file_contents("src/test-files/bad-start.csv").unwrap()).unwrap();
     }
 
     #[test]
     #[should_panic]
     fn bad_end() {
-        let _ = read_file(get_file_contents("src/test-files/bad-end.csv").unwrap()).unwrap();
+        let _ = process_file(get_file_contents("src/test-files/bad-end.csv").unwrap()).unwrap();
     }
 
     #[test]
     #[should_panic]
     fn bad_trans_label() {
-        let _ = read_file(get_file_contents("src/test-files/bad-trans-label.csv").unwrap()).unwrap();
+        let _ = process_file(get_file_contents("src/test-files/bad-trans-label.csv").unwrap()).unwrap();
     }
 
     #[test]
     fn one_account() {
         let a = get_file_contents("src/test-files/1-account.csv").unwrap();
-        assert_eq!(&format!("{}", read_file(a).unwrap().get(0).unwrap()),
+        assert_eq!(&format!("{}", process_file(a).unwrap().get(0).unwrap()),
                    &format!("Name: Savings | Balance: $1.0\n\
                    Transactions:\n\
                    Date: {:?} | Label: a | Amount: $1.0",
@@ -136,7 +139,7 @@ mod tests {
 
     #[test]
     fn two_accounts() {
-        let b = read_file(get_file_contents("src/test-files/2-account.csv").unwrap()).unwrap();
+        let b = process_file(get_file_contents("src/test-files/2-account.csv").unwrap()).unwrap();
         assert_eq!(&format!("{}", b.get(0).unwrap()),
                    &format!("Name: Savings | Balance: $1.0\n\
                    Transactions:\n\
